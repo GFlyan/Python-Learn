78290a849805b4fecca3144f94b9b57454ae8ff1
Flyan <guilhermeflyan.22@gmail.com>
2024-04-19 14:54:16 -0300

    Create Ex090.py

---
+++ Ex090.py
@@ -0,0 +1,30 @@
+##Variáveis Compostas - Dicionários
+
+#Dicionários sao variáveis compostas que armazenam dados de forma semelhante à listas e tuplas,
+#entretanto suas posições referênciais são diferentes, sendo que em listas/tuplas
+#as posições são referênciais númericos, nos dicionários as posições são referênciais
+#literais.
+
+#Podem ser representados por dict() ou {}
+
+##Values (valores) -> <dict>.values()
+
+#É o valor atribuido à uma posição literal
+
+##Keys (chaves) -> <dict>.keys()
+
+#São as próprias posições literais
+
+##Items (itens) -> <dict>.items()
+
+#É o conjunto das chaves e valores
+
+##Adicionando elementos em um dicionário
+
+#Para adicionar posições literais(keys) e valores em um dicionário, não utilizamos nenhuma função
+#diferentemente das listas.
+
+#Utilizamos a seguinte sintaxe:
+
+#<dict>[position] = value
+
