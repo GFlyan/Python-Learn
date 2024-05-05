85426942e16e9c69023bf7762ad4d30c6c097a93
GFlyan <guilhermeflyan.22@gmail.com>
2024-05-05 16:19:31 -0300

    Adding Completed Exercises
    I've added all Python Exercises I've completed.

---
+++ Ex101.py
@@ -0,0 +1,37 @@
+'''
+Interactive Help 
+
+-> É utilizado para saber como utilizar uma função
+-> help(x) ou print(x.__doc__)
+
+Docstrings
+
+-> É a documentação que deve ser posta em uma função
+-> Para criar a documentação(docstring) deve-se utilizar 3 áspas duplas (""")
+   no incio e no fim.
+
+Parâmetros Opcionais
+
+-> Uma função pode ser definida com parâmetros opcionais:
+
+    def function (x, y=z)
+
+    Na função function, x é um parâmetro obrigatório e y é um parâmetro opcional que
+    se caso y não seja fornecido quando a função for utilizada, ele irá receber o valor
+    z.
+
+-> Para definição de um parâmetro opcional deve-se utilizar <param>=...
+
+Escopo de Variáveis
+
+Variável Local -> Uma variável local existe somente dentro de uma função
+
+Variável Global -> Uma variável global existe fora e dentro de uma função (ou seja, 
+                   em todas as ocasiões possíveis de utilização para a mesma)
+
+Retornando Valores
+
+return <...> -> O return é utilizado para retornar algo, podendo ser qualquer tipo de valor
+                ou variável, desde que estabelecido na função.
+'''
+
