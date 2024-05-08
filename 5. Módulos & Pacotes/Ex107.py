033b5f9a255efa627a2ec59aaa0de9e7a67aca83
GFlyan <guilhermeflyan.22@gmail.com>
2024-05-08 13:11:52 -0300

    Adding Completed Exercises
    I've added all Python exercises I've completed.

---
+++ 5. Módulos & Pacotes/Ex107.py
@@ -0,0 +1,19 @@
+'''
+Modularização
+
+-> Tem foco principal em dividir um sistema grande no intuito de aumentar a organização e a legibilidade do código facilitando a manutenção.
+ 
+-> Todo arquivo .py dentro do projeto pode ser potencialmente um módulo.
+
+Pacotes
+
+-> Tem foco de organizar os módulos em diferentes vertentes.
+
+-> Toda pasta no mesmo arquivo do projeto principal pode ser potencialmente um pacote.
+
+'''
+
+#Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+
+#Faça também um programa que importe esse módulo e use algumas dessas funções.
+
---
+++ 5. Módulos & Pacotes/Ex109.py
@@ -0,0 +1,1 @@
+#Modifique as funções que foram craidas no Ex107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvido no Ex108
